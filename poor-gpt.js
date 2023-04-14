fc832de63ce7d773632d4923819d4543c50773d7
eggcaker <eggcaker@gmail.com>
2023-04-14 13:35:53 +0800

    add poor-gpt.js

---
+++ poor-gpt.js
@@ -0,0 +1,39 @@
+const puppeteer = require('puppeteer');
+const { convert } = require('html-to-text');
+
+
+const args = process.argv.slice(2);
+const query = args.join(' ');
+
+const options = {
+  wordwrap: 130,
+};
+
+(async () => {
+  const browser = await puppeteer.connect({ browserURL: 'http://127.0.0.1:9999/json', defaultViewport: null });
+  const pages = await browser.pages();
+
+  const chatGPTUrl = 'https://chat.openai.com/';
+
+  let page = pages.filter(p => p.url() === chatGPTUrl)[0];
+  if (page) {
+    await page.type('textarea', query);
+    await page.keyboard.press('Enter');
+
+    await page.waitForFunction(() => {
+      const element = Array.from(document.querySelectorAll('div')).find(el => el.textContent.includes('Regenerate response'));
+      if (element) {
+        return true;
+      }
+      return false;
+    }, { timeout: 30000 });
+
+    const searchResults = await page.$$eval('div.markdown', elements => elements.map(el => el.innerHTML));
+    console.log(convert(searchResults[searchResults.length - 1], options));
+  } else {
+    console.log('ChatGPT tab not found.');
+  }
+
+  browser.disconnect();
+})();
+
